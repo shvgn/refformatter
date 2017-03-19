3c99d1537c68d9579573c7fe4d4731c95198d360
Evgeny Shevchenko <shvgn@protonmail.ch>
2017-03-19 18:25:36 +0300

    Reimplement Reference component as class

--- src/components/constructor/Reference.js
+++ src/components/constructor/Reference.js
@@ -1,20 +1,29 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-function Reference (props) {
-  console.log(props)
-  const {title, author, volume, page, issue, issued} = props
-  console.log('author', author)
+class Reference extends Component {
+  render () {
+    const {title, author, volume, page, issue, issued} = this.props
+    console.log('author', author)
 
-  // Journal title
-  const shortContainerTitle = props['short-container-title']
+    // Journal title
+    const shortContainerTitle = this.props['short-container-title']
 
-  return (
-    <div>
-      <div>{title}</div>
-      <div>{author.map(a => `${a.given} ${a.family}`).join(', ')}</div>
-      <span><i>{shortContainerTitle}</i> <b>{volume}</b> {issue}, pp. {page}, Issued {issued['date-parts'][0].join('-')}</span>
-    </div>
-  )
+    return (
+      <div>
+        <div>
+          {title}
+        </div>
+        <div>
+          {author.map(a => `${a.given} ${a.family}`).join(', ')}
+        </div>
+        <div>
+          {shortContainerTitle}
+          <b>{volume}</b> {issue}, pp. {page},
+        Issued {issued['date-parts'][0].join('-')}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Reference
